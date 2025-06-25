c8dcb78faf8f0b4d05b487ee7a37712d41e7617a
TracyBachmann <tracy.bachmann68@gmail.com>
2025-06-25 22:29:45 +0200

    feat(docker): mise en place de l'infrastructure Docker avec MongoDB et SQL Server

--- backend/Program.cs
+++ backend/Program.cs
@@ -105,6 +105,7 @@ builder.Services.AddSingleton<IMongoClient>(sp =>
     var settings = sp.GetRequiredService<IOptions<MongoSettings>>().Value;
     return new MongoClient(settings.ConnectionString);
 });
+builder.Services.AddSingleton<MongoLogService>();
 
 // --------------------------------------------------
 // 6) Register application services & background tasks
@@ -116,6 +117,7 @@ builder.Services.AddScoped<StockService>();
 builder.Services.AddScoped<RecommendationService>();
 builder.Services.AddScoped<ReservationCleanupService>();
 builder.Services.AddScoped<HistoryService>();
+builder.Services.AddScoped<LoanReminderService>();
 builder.Services.AddHostedService<LoanReminderBackgroundService>();
 
 // --------------------------------------------------
@@ -165,6 +167,19 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "BiblioMate API v1");
+        c.RoutePrefix = "swagger"; 
+    });
+
+    app.MapGet("/", () => Results.Redirect("/swagger"));
+    
+}
 
 // --------------------------------------------------
 // HTTP request pipeline
